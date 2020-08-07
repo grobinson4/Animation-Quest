b0603ce02af050f21195bfa8dce9517b53e08f5c
Gary D. Robinson <gary.robinson2524@gmail.com>
2020-08-06 23:53:37 -0400

    Added animations to spheres

---
+++ Animation-Quest/Assets/VRRenderScale.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VRRenderScale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRSettings.eyeTextureResolutionScale = 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
